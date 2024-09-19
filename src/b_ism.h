c17dd01a51127b8c105b6829a66e8aad8af3ea73
Jakob <jakobgoldmann@gmx.at>
2024-09-19 18:08:17 +0200

    some prep

--- src/b_ism.h
+++ src/b_ism.h
@@ -5,34 +5,33 @@
 #ifndef CPP_B_ISM_H
 #define CPP_B_ISM_H
 
+#include <Eigen/src/Cholesky/LDLT.h>
+#include <Rcpp.h>
+#include <RcppEigen.h>
 
 #include <Eigen/Core>
-#include <Rcpp.h>
-#include <set>
-#include <iostream>
 #include <Eigen/Dense>
-#include <Eigen/src/Cholesky/LDLT.h>
-#include <random>
-#include <RcppEigen.h>
 #include <algorithm>
 #include <cstdint>
 #include <iomanip>
+#include <iostream>
+#include <random>
+#include <set>
+
 #include "./model_selection/ms_base.h"
-#include "mombf/cstat.h"
 #include "model_selection/ms_no_optimization.h"
-#include "model_selection/ms_split_z.h"
 #include "model_selection/ms_parallel_z.h"
+#include "model_selection/ms_split_z.h"
+#include "mombf/cstat.h"
 #include "progressbar.h"
+
+// [[Rcpp::depends(RcppEigen)]]
 
 enum ModelSelectionVersion {
     NO_OPTIMIZATION,
     SPLIT_MATRIX,
     SPLIT_MATRIX_PARALLEL
 };
-
-
-// [[Rcpp::depends(RcppEigen)]]
-
 
 struct BismResults {
     Eigen::MatrixXd b_store;
@@ -43,53 +42,50 @@ struct BismResults {
     Eigen::MatrixXd w_pip_store;
 };
 
-
 void b_ism(
-        const Eigen::MatrixXd &data,
-        bool include_constant,
-        bool tfe,
-        bool ife,
-        bool iis,
-        bool sis,
-        int y_index,
-        int i_index,
-        int t_index,
-        long Ndraw,
-        long Nburn,
-        std::string b_prior,
-        double lambda_b,
-        double c0,
-        double C0,
-        bool geweke,
-        BismResults &results,
-        ModelSelectionVersion model_selection_version
-);
+    const Eigen::MatrixXd &data,
+    bool include_constant,
+    bool tfe,
+    bool ife,
+    bool iis,
+    bool sis,
+    int y_index,
+    int i_index,
+    int t_index,
+    long Ndraw,
+    long Nburn,
+    std::string b_prior,
+    double lambda_b,
+    double c0,
+    double C0,
+    bool geweke,
+    BismResults &results,
+    ModelSelectionVersion model_selection_version);
 
 Rcpp::List b_ism_wrapper(
-        Rcpp::NumericMatrix data,
-        bool include_constant,
-        bool tfe,
-        bool ife,
-        bool iis,
-        bool sis,
-        int y_index,
-        int i_index,
-        int t_index,
-        long Ndraw,
-        long Nburn,
-        std::string b_prior,
-        double lambda_a,
-        double lambda_b,
-        double lambda_g,
-        double c0,
-        double C0,
-        double oa,
-        double ob,
-        double va,
-        double vb,
-        double pip_thr,
-        bool w_sis_rand,
-        bool geweke
-);
+    Rcpp::NumericMatrix data,
+    bool include_constant,
+    bool tfe,
+    bool ife,
+    bool iis,
+    bool sis,
+    int y_index,
+    int i_index,
+    int t_index,
+    long Ndraw,
+    long Nburn,
+    std::string b_prior,
+    double lambda_a,
+    double lambda_b,
+    double lambda_g,
+    double c0,
+    double C0,
+    double oa,
+    double ob,
+    double va,
+    double vb,
+    double pip_thr,
+    bool w_sis_rand,
+    bool geweke);
 
-#endif //CPP_B_ISM_H
+#endif  // CPP_B_ISM_H
